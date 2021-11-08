6c47da23f12d0420ddd4225288aef992b157c49b
Rober P <robertopaletini@gmail.com>
2021-11-08 16:05:57 -0300

    First Commit

---
+++ src/pages/index.js
@@ -0,0 +1,24 @@
+import React, {useState} from 'react'
+import HeroSection from '../components/HeroSection'
+import Navbar from '../components/Navbar'
+import Sidebar from '../components/Sidebar'
+import Image from '../images/hero.jpg'
+
+const Home = () => {
+    const [isOpen, setIsOpen] = useState(false)
+
+    const toggle = () => {
+        setIsOpen(!isOpen)
+    }
+
+    return (
+        <>
+            <Sidebar isOpen={isOpen} toggle={toggle} />
+            <Navbar toggle={toggle} />
+ 
+            <HeroSection />
+        </>
+    )
+}
+
+export default Home
